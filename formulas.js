dcffdebeb43bae6da66fe1bb92bbd99ccfca033b
Barrera12373 <arce.12373@gmail.com>
2022-10-17 20:26:31 -0500

    4

--- formulas.js
+++ formulas.js
@@ -107,3 +107,98 @@ function Obtenercuadrado(){
     areaResultado.innerHTML = "" + area;
     perimetroResultado.innerHTML = "" + perimetro;
   }
+  function Obtenerpoligonoregular(){
+    var areaResultado = document.getElementById("areaResultado");
+    var perimetroResultado = document.getElementById("perimetroResultado");
+    var P = parseInt(document.getElementById("perimetro").value);
+    var a = parseInt(document.getElementById("apotema").value);
+    var n = parseInt(document.getElementById("numerolado").value);
+    var b = parseInt(document.getElementById("base").value);
+  
+    var area = (P*a)/2;
+    var perimetro=(n*b);
+  
+    areaResultado.innerHTML = "" + area;
+    perimetroResultado.innerHTML = "" + perimetro;
+  }
+  function Obtenercoronacircular(){
+    var areaResultado = document.getElementById("areaResultado");
+    var R = parseInt(document.getElementById("radioma").value);
+    var r = parseInt(document.getElementById("radiome").value);
+  
+    var area = (Math.PI*R**2-r**2);
+  
+    areaResultado.innerHTML = "" + area;
+  }
+  function Obtenersectorcircular(){
+    var areaResultado = document.getElementById("areaResultado");
+    var R = parseInt(document.getElementById("radio").value);
+    var n = parseInt(document.getElementById("numerogrados").value);
+  
+    var area = (Math.PI*R**2*n)/360;
+  
+    areaResultado.innerHTML = "" + area;
+  }
+  function Obtenercubo(){
+    var areaResultado = document.getElementById("areaResultado");
+    var volumenResultado = document.getElementById("volumenResultado");
+    var a = parseInt(document.getElementById("ladoa").value);
+  
+    var area = (6*a**2);
+    var volumen = (a**3);
+  
+    areaResultado.innerHTML = "" + area;
+    volumenResultado.innerHTML = "" + volumen;
+  }
+  function Obtenercilindro(){
+    var areaResultado = document.getElementById("areaResultado");
+    var volumenResultado = document.getElementById("volumenResultado");
+    var h = parseInt(document.getElementById("altura").value);
+    var R = parseInt(document.getElementById("radio").value);
+  
+    var area = (2*Math.PI*R*(h+R));
+    var volumen = (Math.PI*R**2*h);
+  
+    areaResultado.innerHTML = "" + area;
+    volumenResultado.innerHTML = "" + volumen;
+  }
+  function Obtenerortoedro(){
+    var areaResultado = document.getElementById("areaResultado");
+    var volumenResultado = document.getElementById("volumenResultado");
+    var a = parseInt(document.getElementById("ladoa").value);
+    var b = parseInt(document.getElementById("ladob").value);
+    var c = parseInt(document.getElementById("ladoc").value);
+  
+    var area = (2*(a*b+a*c+b*c));
+    var volumen = (a*b*c);
+  
+    areaResultado.innerHTML = "" + area;
+    volumenResultado.innerHTML = "" + volumen;
+  }
+  function Obtenerprismarecto(){
+    var areaResultado = document.getElementById("areaResultado");
+    var volumenResultado = document.getElementById("volumenResultado");
+    var a = parseInt(document.getElementById("ladoa").value);
+    var h = parseInt(document.getElementById("altura").value);
+    var P = parseInt(document.getElementById("perimetro").value);
+    var A = parseInt(document.getElementById("areab").value);
+  
+    var area = (P*(h+a));
+    var volumen = (A*h);
+  
+    areaResultado.innerHTML = "" + area;
+    volumenResultado.innerHTML = "" + volumen;
+  }
+  function Obtenercono(){
+    var areaResultado = document.getElementById("areaResultado");
+    var volumenResultado = document.getElementById("volumenResultado");
+    var g = parseInt(document.getElementById("generatriz").value);
+    var h = parseInt(document.getElementById("altura").value);
+    var R = parseInt(document.getElementById("radio").value);
+  
+    var area = (Math.PI*R*(R+g));
+    var volumen = (Math.PI*R**2*h)/3;
+  
+    areaResultado.innerHTML = "" + area;
+    volumenResultado.innerHTML = "" + volumen;
+  }
